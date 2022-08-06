9b464fbcc16f78af6bd863163cb41ecf6fb77c39
Abhishek <you@example.com>
2022-08-06 13:11:47 +0530

    Modified [Abhishek]: Added player1 Start position

--- src/com/bridgelabz/snakeNladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeNladder/SnakeAndLadder.java
@@ -3,9 +3,14 @@ package com.bridgelabz.snakeNladder;
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
-		
 		System.out.println(" ***Welcome to Snake & Ladder Simulation Program*** ");
-
+        
+		/*UC1
+		 * player1's start position
+		 * */
+		
+		int position = 0;
+		System.out.println("Player1's position: " +position);
 	}
 
 }
