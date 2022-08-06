2bb4249ae16fbd36211732f9fa77e78c7673a241
Abhishek <you@example.com>
2022-08-06 13:38:10 +0530

    Modified [Abhishek] Added UC3 Checking options

--- src/com/bridgelabz/snakeNladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeNladder/SnakeAndLadder.java
@@ -8,12 +8,32 @@ public class SnakeAndLadder {
 		int position = 0;
 		System.out.println("Player1's position: " +position);
 		
-		/*UC2
-		 * Rolling the dice to get a number
-		 * */
-		
 		int dice = (int)(Math.random()*10)%6 + 1; 
 		System.out.println("Dice roll result is: " +dice);
+		
+		/*UC3
+		 * Checking options for Player1
+		 * */
+		
+		int option = (int)(Math.random() * 10 ) % 3;
+		System.out.println("Option for Player1: "+option);
+		switch (option) {
+		case 1:
+			System.out.println("Ladder");
+			position += dice;
+			break;
+		case 2:
+			System.out.println("Snake");
+			position -= dice;
+			if(position<0) {
+				position = 0;
+			}
+			break;
+		default:
+			System.out.println("No Play");
+		}
+		System.out.println("Position of Player1: " + position);
+
 	}
 
 }
