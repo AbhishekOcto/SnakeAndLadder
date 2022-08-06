839d086fa749bad88b2571a1cf85dc9afe7206f1
Abhishek <you@example.com>
2022-08-06 13:16:34 +0530

    Modified [Abhishek]: Added Dice roll And get a number

--- src/com/bridgelabz/snakeNladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeNladder/SnakeAndLadder.java
@@ -4,13 +4,16 @@ public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		System.out.println(" ***Welcome to Snake & Ladder Simulation Program*** ");
-        
-		/*UC1
-		 * player1's start position
-		 * */
-		
+       
 		int position = 0;
 		System.out.println("Player1's position: " +position);
+		
+		/*UC2
+		 * Rolling the dice to get a number
+		 * */
+		
+		int dice = (int)(Math.random()*10)%6 + 1; 
+		System.out.println("Dice roll result is: " +dice);
 	}
 
 }
