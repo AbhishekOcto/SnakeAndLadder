f3ec9e69764a5c305f0e3cf2f4d59833d60501a9
Abhishek <you@example.com>
2022-08-06 12:57:51 +0530

    Added [Abhishek]: Welcome to Snake and ladder

---
+++ src/com/bridgelabz/snakeNladder/SnakeAndLadder.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.snakeNladder;
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+		
+		System.out.println(" ***Welcome to Snake & Ladder Simulation Program*** ");
+
+	}
+
+}
